a17e7a63ae32d7e1039f7e2191ffcd1e92a130a5
Kamil <mentos.96@o2.pl>
2023-06-22 21:47:15 +0200

    Write class Lumberjack and enum Flpajack

---
+++ Lumberjack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupOfLumberjacks
+{
+    internal class Lumberjack
+    {
+        //private field Name
+        private string name;
+        //reference Stack he take Enum EFlpajack
+        private Stack<EFlapjack> flapjackStack;
+        //property to set privet field name
+        public string Name 
+        { 
+            get { return name; } 
+            private set { name = value; }
+        }
+        //Constructor to blid object
+        public Lumberjack(string name)
+        {
+            Name = name;
+            flapjackStack = new Stack<EFlapjack>();
+        }
+        //method to place emenent on top the stack
+        public void TakeFlapjack(EFlapjack flapjack)
+        {
+            flapjackStack.Push(flapjack);
+        }
+        //method remove element from the stack
+        public void EatFlapjacks()
+        {
+            Console.WriteLine($"{Name} je {flapjackStack.Pop()} nalesnik");
+        }
+        //override method to beter understed when debug program
+        public override string ToString()
+        {
+            return $"{Name} ma nalesnikow {flapjackStack.Count}";
+        }
+
+    }
+}
